4ed9f8a51f9633cd2b2bba1029707bec42d09d0d
TwanMulder <twan.mulder@gmail.com>
2019-12-27 21:45:17 +0100

    Basic navigation setup

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.scss";
 
 class Navigation extends React.Component {
   render() {
     return (
-      <nav>
-        <h1>Navigation</h1>
-      </nav>
+      <header>
+        <nav>
+          <a>
+            <img src=""></img>
+            LOGO
+          </a>
+          <ul>
+            <li>
+              <Link to="#services">Services</Link>
+            </li>
+            <li>
+              <Link to="#clients">Clients</Link>
+            </li>
+            <li>
+              <Link to="#careers">Careers</Link>
+            </li>
+            <li>
+              <Link to="#about">About</Link>
+            </li>
+            <li>
+              <Link to="#contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
