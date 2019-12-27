dd8fd355ed263e9a8f803ec2b6812b948d7af020
TwanMulder <twan.mulder@gmail.com>
2019-12-27 21:29:32 +0100

    Created Navigation component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navigation.scss";
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <nav>
+        <h1>Navigation</h1>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
