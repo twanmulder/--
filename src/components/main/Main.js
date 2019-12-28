a34c4fd8a91429e29a54299046c7dccc6b299bb6
TwanMulder <twan.mulder@gmail.com>
2019-12-28 12:48:26 +0100

    Redid hero image styling

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,15 +4,15 @@ class Main extends React.Component {
   render() {
     return (
       <main>
-        <section className="header">
-          <div className="header-textwrapper">
-            <div className="header-text">
+        <section className="hero">
+          <div className="hero-textwrapper">
+            <div className="hero-text">
               <h4>Featured Project</h4>
               <h1>Featured Project Title</h1>
               <a href="#exploremore">Explore more</a>
             </div>
           </div>
-          <div className="header-image"></div>
+          <div className="hero-image"></div>
         </section>
       </main>
     )
