d90d9842bb170d05e3a0037bd6cdc64c15cff265
TwanMulder <twan.mulder@gmail.com>
2019-12-28 13:59:21 +0100

    Created Footer init

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Navigation from "../navigation/Navigation"
 import Main from "../main/Main"
+import Footer from "../footer/Footer"
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <React.Fragment>
         <Navigation></Navigation>
         <Main></Main>
+        <Footer></Footer>
       </React.Fragment>
     )
   }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <ul>
+          <li>
+            <a></a>
+          </li>
+          <li>
+            <a></a>
+          </li>
+          <li>
+            <a></a>
+          </li>
+        </ul>
+      </footer>
+    )
+  }
+}
+
+export default Footer
