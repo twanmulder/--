ffaf3dc3642e88a441efa61cd92336a05c4d4195
TwanMulder <twan.mulder@gmail.com>
2019-12-30 21:57:40 +0100

    Changed Footer link to React Link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Footer() {
   const currentYear = new Date().getFullYear()
@@ -15,7 +16,7 @@ function Footer() {
           </a>
         </li>
         <li>
-          <a href="/">Privacy Policy</a>
+          <Link to="/">Privacy Policy</Link>
         </li>
       </ul>
     </footer>
