e4ff751f5c5a45dbbafded3bdc2349db0955e1d1
TwanMulder <twan.mulder@gmail.com>
2019-12-30 20:46:22 +0100

    Rewrote navigation to function component
    First time using React Hooks!

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,98 +1,88 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
-class Navigation extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showHideNav: false,
-    }
+function Navigation() {
+  const [isNavVisible, setNavState] = useState(false)
+
+  const toggleNav = () => {
+    setNavState(!isNavVisible)
   }
 
-  toggleNav = () => {
-    this.setState(prevState => ({
-      showHideNav: !prevState.showHideNav,
-    }))
-  }
+  return (
+    <header>
+      {/* MOBILE NAVIGATION */}
+      <nav className=" navigation navigation--mobile">
+        <a className="navigation-logo">
+          <img src=""></img>
+          LOGO
+        </a>
+        {/* HAMBURGER ICON */}
+        <svg
+          onClick={toggleNav}
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M19 2.14193H0V0.996094H19V2.14193ZM19 8.57552H0V7.42969H19V8.57552ZM0 15.0013H19V13.8555H0V15.0013Z"
+            fill="black"
+          ></path>
+        </svg>
+        <ul
+          className={
+            isNavVisible
+              ? " navigation-list navigation-list--open"
+              : "navigation-list navigation-list--closed"
+          }
+        >
+          <li className="navigation-item">
+            <Link to="#services">Services</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="#clients">Clients</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="#careers">Careers</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="#about">About</Link>
+          </li>
+          <li className="navigation-item navigation-item--highlight">
+            <Link to="#contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
 
-  render() {
-    const isNavShown = this.state.showHideNav
-    return (
-      <header>
-        {/* MOBILE NAVIGATION */}
-        <nav className=" navigation navigation--mobile">
-          <a className="navigation-logo">
-            <img src=""></img>
-            LOGO
-          </a>
-          {/* HAMBURGER ICON */}
-          <svg
-            onClick={this.toggleNav}
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M19 2.14193H0V0.996094H19V2.14193ZM19 8.57552H0V7.42969H19V8.57552ZM0 15.0013H19V13.8555H0V15.0013Z"
-              fill="black"
-            ></path>
-          </svg>
-          <ul
-            className={
-              isNavShown
-                ? " navigation-list navigation-list--open"
-                : "navigation-list navigation-list--closed"
-            }
-          >
-            <li className="navigation-item">
-              <Link to="#services">Services</Link>
-            </li>
-            <li className="navigation-item">
-              <Link to="#clients">Clients</Link>
-            </li>
-            <li className="navigation-item">
-              <Link to="#careers">Careers</Link>
-            </li>
-            <li className="navigation-item">
-              <Link to="#about">About</Link>
-            </li>
-            <li className="navigation-item navigation-item--highlight">
-              <Link to="#contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* DESKTOP NAVIGATION */}
-        <nav className=" navigation navigation--desktop">
-          <a className="navigation-logo">
-            <img src=""></img>
-            LOGO
-          </a>
-          <ul className="navigation-list">
-            <li className="navigation-item">
-              <Link to="#services">Services</Link>
-            </li>
-            <li className="navigation-item">
-              <Link to="#clients">Clients</Link>
-            </li>
-            <li className="navigation-item">
-              <Link to="#careers">Careers</Link>
-            </li>
-            <li className="navigation-item">
-              <Link to="#about">About</Link>
-            </li>
-            <li className="navigation-item navigation-item--highlight">
-              <Link to="#contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
-  }
+      {/* DESKTOP NAVIGATION */}
+      <nav className=" navigation navigation--desktop">
+        <a className="navigation-logo">
+          <img src=""></img>
+          LOGO
+        </a>
+        <ul className="navigation-list">
+          <li className="navigation-item">
+            <Link to="#services">Services</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="#clients">Clients</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="#careers">Careers</Link>
+          </li>
+          <li className="navigation-item">
+            <Link to="#about">About</Link>
+          </li>
+          <li className="navigation-item navigation-item--highlight">
+            <Link to="#contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Navigation
