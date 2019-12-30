7e1f340910a9fb11a72e6ac4c9b9735db66ad65a
TwanMulder <twan.mulder@gmail.com>
2019-12-30 20:38:52 +0100

    Rewrote Footer to fuction component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,23 +1,21 @@
 import React from "react"
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <ul>
-          <li>
-            <a></a>
-          </li>
-          <li>
-            <a></a>
-          </li>
-          <li>
-            <a></a>
-          </li>
-        </ul>
-      </footer>
-    )
-  }
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a></a>
+        </li>
+        <li>
+          <a></a>
+        </li>
+        <li>
+          <a></a>
+        </li>
+      </ul>
+    </footer>
+  )
 }
 
 export default Footer
