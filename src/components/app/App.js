7da50a19bd5ac17d8608f04affc466cd32a22ef2
TwanMulder <twan.mulder@gmail.com>
2019-12-30 20:37:54 +0100

    Rewrote App to function component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,16 +4,14 @@ import Navigation from "../navigation/Navigation"
 import Main from "../main/Main"
 import Footer from "../footer/Footer"
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navigation></Navigation>
-        <Main></Main>
-        <Footer></Footer>
-      </React.Fragment>
-    )
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <Navigation></Navigation>
+      <Main></Main>
+      <Footer></Footer>
+    </React.Fragment>
+  )
 }
 
 export default App
