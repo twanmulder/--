5792e60d6c81f99dfac67d4e337e95c65233cbad
TwanMulder <twan.mulder@gmail.com>
2019-12-31 10:30:19 +0100

    Started working on USP component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import Hero from "../hero/Hero"
 import HeroSugg from "../heroSugg/HeroSugg"
+import USP from "../usp/USP"
 
 function Main() {
   return (
     <main>
       {/* <Hero /> */}
       <HeroSugg />
+      <USP />
     </main>
   )
 }
---
+++ src/components/usp/USP.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+function USP() {
+  return (
+    <section className="section-usp">
+      <div className="usp-wrapper">
+        <article className="usp">
+          <div className="usp-image"></div>
+          <div className="usp-textWrapper">
+            <h4 className="usp-title"> We can do this for you</h4>
+            <p className="usp-text">
+              We're really good at doing this certain thing, it's true!
+            </p>
+          </div>
+        </article>
+
+        <article className="usp">
+          <div className="usp-image"></div>
+          <div className="usp-textWrapper">
+            <h4 className="usp-title"> We can do this for you</h4>
+            <p className="usp-text">
+              We're really good at doing this certain thing, it's true!
+            </p>
+          </div>
+        </article>
+
+        <article className="usp">
+          <div className="usp-image"></div>
+          <div className="usp-textWrapper">
+            <h4 className="usp-title"> We can do this for you</h4>
+            <p className="usp-text">
+              We're really good at doing this certain thing, it's true!
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default USP
