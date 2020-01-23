bbdf9d565febe5a3268bb2f092dd604a0d142687
TwanMulder <twan.mulder@gmail.com>
2020-01-23 21:21:54 +0100

    Added cool animation

--- src/components/heroSugg/HeroSugg.js
+++ src/components/heroSugg/HeroSugg.js
@@ -1,26 +1,47 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-function HeroSugg() {
-  return (
-    <section className="hero">
-      <article className="hero-text">
-        <h1>Unique Value Proposition</h1>
-        <h4>
-          Reason why we are better than all the others but a little longer
-        </h4>
-      </article>
-      <div className="hero-CTAs">
-        <Link className="hero-CTA hero-CTA--primary" to="/">
-          Get in contact
-        </Link>
-        <Link className="hero-CTA hero-CTA--secondary" to="/">
-          Learn more
-        </Link>
-      </div>
-      <div className="hero-image"></div>
-    </section>
-  )
+import { TimelineMax, Circ } from "gsap/all"
+import "gsap/CSSPlugin"
+
+class HeroSugg extends React.Component {
+  componentDidMount() {
+    console.log("test")
+  }
+
+  render() {
+    return (
+      <section className="hero">
+        <article className="hero-text">
+          <h1>
+            <div className="word-container">
+              <span>Unique&nbsp;</span>
+            </div>
+
+            <div className="word-container">
+              <span>Value&nbsp;</span>
+            </div>
+
+            <div className="word-container">
+              <span>Proposition&nbsp;</span>
+            </div>
+          </h1>
+          <h4>
+            Reason why we are better than all the others but a little longer
+          </h4>
+        </article>
+        <div className="hero-CTAs">
+          <Link className="hero-CTA hero-CTA--primary" to="/">
+            Get in contact
+          </Link>
+          <Link className="hero-CTA hero-CTA--secondary" to="/">
+            Learn more
+          </Link>
+        </div>
+        <div className="hero-image"></div>
+      </section>
+    )
+  }
 }
 
 export default HeroSugg
